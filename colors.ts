852de108880737be0b64228141ce141985161801
hyeju1123 <hjjun1123@gmail.com>
2024-06-01 13:36:33 +0900

    feat: Set custom theme colors

---
+++ colors.ts
@@ -0,0 +1,19 @@
+interface Theme {
+  light: {
+    background: string;
+    tagBg: string;
+    codeBg: string;
+    text: string;
+    textEmph: string;
+  };
+}
+
+export const theme: Theme = {
+  light: {
+    background: "#FFFFFF",
+    tagBg: "#E4F0FB",
+    codeBg: "#32435F",
+    text: "#486084",
+    textEmph: "#F8F1AE",
+  },
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,9 @@
 import type { Config } from "tailwindcss";
+import { theme } from "./colors";
+
+const {
+  light: { background, tagBg, codeBg, text, textEmph },
+} = theme;
 
 const config: Config = {
   content: [
@@ -7,6 +12,20 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    colors: {
+      background: background,
+      tagBg: tagBg,
+      codeBg: codeBg,
+      text: text,
+      textEmph: textEmph,
+    },
     extend: {},
   },
   plugins: [],
